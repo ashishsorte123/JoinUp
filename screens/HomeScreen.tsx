3ca840b0690935ff7528adc33b16d6a2dec7377d
ashishsorte123 <88495557+ashishsorte123@users.noreply.github.com>
2022-03-17 00:17:29 +0530

    home screen

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import ChatRoomItem from "../components/ChatRoomItem";
+
+import chatRoomsData from "../assets/dummy-data/ChatRooms";
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.page}>
+      <FlatList
+        data={chatRoomsData}
+        renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: "white",
+    flex: 1,
+  },
+});
