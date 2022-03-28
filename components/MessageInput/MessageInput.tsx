1dac7e36d4745acef46a3078c23ddf7ddeb01f28
ashishsorte123 <88495557+ashishsorte123@users.noreply.github.com>
2022-03-28 19:03:51 +0530

    Image Upload

--- components/MessageInput/MessageInput.tsx
+++ components/MessageInput/MessageInput.tsx
@@ -1,5 +1,12 @@
-import { View, StyleSheet, TextInput, Pressable } from "react-native";
-import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  Platform,
+  Image,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import {
   AntDesign,
   Feather,
@@ -7,14 +14,34 @@ import {
   MaterialCommunityIcons,
   SimpleLineIcons,
 } from "@expo/vector-icons";
-import { Auth, DataStore } from "aws-amplify";
+import { Auth, DataStore, Storage } from "aws-amplify";
 import { Message } from "../../src/models";
 import { ChatRoom } from "../../src/models";
 import EmojiSelector from "react-native-emoji-selector";
+import * as ImagePicker from "expo-image-picker";
+import uuid from "react-native-uuid";
 
 const MessageInput = ({ chatRoom }) => {
   const [message, setMessage] = useState("");
   const [isEmojiPickerOpen, setIsEmojiPickerOpen] = useState(false);
+  const [image, setImage] = useState<string | null>(null);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const libraryResponse =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        const photoResponse = await ImagePicker.requestCameraPermissionsAsync();
+        if (
+          libraryResponse.status !== "granted" ||
+          photoResponse.status !== "granted"
+        ) {
+          alert("Sorry , we need camera roll permissions to make this work !");
+        }
+      }
+    })();
+  }, []);
 
   const sendMessage = async () => {
     //send message
@@ -27,8 +54,7 @@ const MessageInput = ({ chatRoom }) => {
       })
     );
     updateLastMessage(newMessage);
-    setMessage("");
-    setIsEmojiPickerOpen(false);
+    resetFields();
   };
 
   const updateLastMessage = async (newMessage) => {
@@ -44,15 +70,125 @@ const MessageInput = ({ chatRoom }) => {
   };
 
   const onPress = () => {
-    if (message) {
+    if (image) {
+      sendImage();
+    } else if (message) {
       sendMessage();
     } else {
       onPlusClicked();
     }
   };
 
+  const resetFields = () => {
+    setMessage("");
+    setIsEmojiPickerOpen(false);
+    setImage(null);
+    setProgress(0);
+    // setSoundURI(null);
+    // removeMessageReplyTo();
+  };
+
+  // Image Picker
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const takePhoto = async () => {
+    const result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      aspect: [4, 3],
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const progressCallback = (progress) => {
+    setProgress(progress.loaded / progress.total);
+  };
+
+  const sendImage = async () => {
+    if (!image) {
+      return;
+    }
+    const blob = await getImageBlob();
+    const { key } = await Storage.put(`${uuid.v4()}.jpeg`, blob, {
+      progressCallback,
+    });
+
+    // send message
+    const user = await Auth.currentAuthenticatedUser();
+    const newMessage = await DataStore.save(
+      new Message({
+        content: message,
+        image: key,
+        userID: user.attributes.sub,
+        chatroomID: chatRoom.id,
+      })
+    );
+
+    updateLastMessage(newMessage);
+    resetFields();
+  };
+
+  const getImageBlob = async () => {
+    if (!image) {
+      return null;
+    }
+    const response = await fetch(image);
+    const blob = await response.blob();
+    return blob;
+  };
+
   return (
     <View style={[styles.root, { height: isEmojiPickerOpen ? "50%" : "auto" }]}>
+      {image && (
+        <View style={styles.sendImageContainer}>
+          <Image
+            source={{ uri: image }}
+            style={{ width: 100, height: 100, borderRadius: 10 }}
+          />
+
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "flex-start",
+              alignSelf: "flex-end",
+            }}
+          >
+            <View
+              style={{
+                height: 10,
+                backgroundColor: "#3777f0",
+                borderRadius: 5,
+                width: `${progress * 100}%`,
+              }}
+            />
+          </View>
+
+          <Pressable onPress={() => setImage(null)}>
+            <AntDesign
+              name="close"
+              size={24}
+              color="black"
+              style={{ margin: 10 }}
+            />
+          </Pressable>
+        </View>
+      )}
       <View style={styles.row}>
         <View style={styles.inputContainer}>
           <Pressable
@@ -74,12 +210,24 @@ const MessageInput = ({ chatRoom }) => {
             placeholder="Message..."
             onChangeText={setMessage}
           />
-          <Feather
-            name="camera"
-            size={24}
-            color="#595959"
-            style={styles.icon}
-          />
+          <Pressable onPress={pickImage}>
+            <Feather
+              name="image"
+              size={24}
+              color="#595959"
+              style={styles.icon}
+            />
+          </Pressable>
+
+          <Pressable onPress={takePhoto}>
+            <Feather
+              name="camera"
+              size={24}
+              color="#595959"
+              style={styles.icon}
+            />
+          </Pressable>
+
           <MaterialCommunityIcons
             name="microphone-outline"
             size={24}
@@ -88,7 +236,7 @@ const MessageInput = ({ chatRoom }) => {
           />
         </View>
         <Pressable onPress={onPress} style={styles.buttonContainer}>
-          {message ? (
+          {message || image ? (
             <Ionicons name="send" size={18} color="white" />
           ) : (
             <AntDesign
@@ -155,6 +303,16 @@ const styles = StyleSheet.create({
 
   row: {
     flexDirection: "row",
+  },
+
+  sendImageContainer: {
+    flexDirection: "row",
+    marginVertical: 10,
+    alignSelf: "stretch",
+    justifyContent: "space-between",
+    borderWidth: 1,
+    borderColor: "lightgrey",
+    borderRadius: 10,
   },
 });
 
